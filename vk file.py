60234e306b1959fad8c69b9587a7e6de0b98d8f6
ANORA\vinoth <vinoth@anorasolutions.com>
2023-03-23 13:40:25 +0530

    pseudocode written for that program

--- vk file.py
+++ vk file.py
@@ -4,3 +4,9 @@ for i in range(0,len(l)):
         if len(l[i])<len(l[j]):
             l[i],l[j]=l[j],l[i]
 print(l[0])
+
+"""
+1.take a list of strings
+2.for loop to iterate and check the condition
+3.after sorting print the first  element
+"""
