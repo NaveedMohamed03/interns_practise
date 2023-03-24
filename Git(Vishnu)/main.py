dce0ee06dfcbd4ad7f24d828bfeda7d47dfec675
ANORA\Vishnu Teja <teja@anorasolutions.com>
2023-03-24 14:22:14 +0530

    remove lines and added three lines

--- Git(Vishnu)/main.py
+++ Git(Vishnu)/main.py
@@ -1,21 +1,3 @@
 def find_first_non_repeating_char(string):
     char_count = {}
     for char in string:
-        if char in char_count:
-            char_count[char] += 1
-        else:
-            char_count[char] = 1
-
-    for char in string:
-        if char_count[char] == 1:
-            return char
-    return None
-string = "Rolls Royce"
-result = find_first_non_repeating_char(string)
-print(result)
-"""
-First write def function 
-Then by using for loop iteration takes place 
-After iteration we give string
-And call that function 
-Finally print the output"""
