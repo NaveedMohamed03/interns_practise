2e8bed69931736513fb0955e25f11c3080fcfcad
ANORA\vinoth <vinoth@anorasolutions.com>
2023-03-27 14:25:10 +0530

    resolved

---
+++ vinothkumar/vinothkumar.py
@@ -0,0 +1,24 @@
+"""
+pseudocode
+1.take a list
+2.initialise su,m=0
+3.iterete using forloop
+add that i into sum
+printsum
+"""
+"""
+question:
+print the largest string in the list
+"""
+
+
+
+l=[1,2,3,4,5]
+sum=0
+for i in l:
+    sum=sum+i
+print(sum)
+
+
+
+
